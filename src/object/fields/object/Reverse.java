486ccf9bb5e2b68962e53a35e1ce38499081e05a
M.J. Azimov <azimovm.j@gmail.com>
2022-01-16 16:48:24 -0600

    First String Java Code to ReverseString Using ByteArray

--- src/object/fields/object/Reverse.java
+++ src/object/fields/object/Reverse.java
@@ -1,19 +1,43 @@
 package object.fields.object;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Locale;
 
+/*
+* Java Program to reverse a word
+*/
+
 public class Reverse {
-   public static void main (String[] args){
+    public static void main(String[] args) {
 
-       String str = "Geeks", nstr = "";
-       char ch;
+        String str = "Geeks", nstr = "";
+        char ch;
 
-       System.out.println("Original Word: ");
-       System.out.println("Geeks"); //Example words
-       for (int i = 0; i<str.length(); i++){
-           ch = str.charAt(i); //Extracts each character
-           nstr = ch+nstr; //Adds each character in front of the existing
-       }
-       System.out.println("Reversed word: " + nstr.toLowerCase(Locale.ROOT));
-   }
+        System.out.println("Original Word: ");
+        System.out.println("Geeks"); //Example words
+        for (int i = 0; i < str.length(); i++) {
+            ch = str.charAt(i); //Extracts each character
+            nstr = ch + nstr; //Adds each character in front of the existing
+        }
+        System.out.println("Reversed word: " + nstr.toLowerCase(Locale.ROOT));
+    }
 }
+   /*
+   * Java Program to ReverceString using ByteArray
+   */
+
+    class ReverseString{
+        public static void main(String[] args) {
+            String input = "GeeksforGeeks";
+            //getByte() method to convert String
+            //into bytes[].
+            byte[] strAsByteArray = input.getBytes();
+            byte[] result = new byte[strAsByteArray.length];
+            //Store result in reverse order into the
+            //result byte[]
+            for (int i = 0; i < strAsByteArray.length; i++){
+                result[i] = strAsByteArray[strAsByteArray.length - i - 1];
+                System.out.println(new String((result)));
+            }
+        }
+    }

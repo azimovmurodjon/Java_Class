fd42d267c4c0a972680ed5cb16bb34f83fe9d8fe
M.J. Azimov <96605136+azimovmurodjon@users.noreply.github.com>
2023-07-06 00:09:25 -0400

    Time String is added

---
+++ src/object/fields/object/TimeString.java
@@ -0,0 +1,14 @@
+package object.fields.object;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class TimeString {
+
+    public static void main(String[] args) {
+        // Format y-M-d or yyyy-MM-d
+        String string = "2023-07-06";
+        LocalDate date = LocalDate.parse(string, DateTimeFormatter.ISO_DATE);
+
+        System.out.println(date);
+    }
+}

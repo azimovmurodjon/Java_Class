705fbdb67c8beb75128e701c2d17966ccf268c1d
Iryna Azimova <iryna.azimova@gmail.com>
2023-09-06 23:18:10 -0400

    Convert Octal To Decimal: Vice versa

--- src/Arrays/Reverce.java
+++ src/Arrays/Reverce.java
@@ -128,3 +128,20 @@ class REV3
         }
     }
 }
+class OctalToDecimal
+{
+    public static void main(String arg[])
+    {
+        int oct, dec=0, i=0, t;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Oct Number : ");
+        oct = sc.nextInt();
+        while(oct != 0)
+        {
+            dec =dec + (oct%10) *(int)Math.pow(8, i);
+            i++;
+            oct = oct/10;
+        }
+        System.out.println("Deci Number is : "+dec);
+    }
+}

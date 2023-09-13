a4498cf350b30e053bbd274d793b05c66d1059ec
M.J. Azimov <96605136+azimovmurodjon@users.noreply.github.com>
2023-09-13 09:56:22 -0400

    Calculate Perimeter Of Rhombus | 3 Ways

--- src/Calculator/Calculator.java
+++ src/Calculator/Calculator.java
@@ -1,5 +1,5 @@
 package Calculator;
-
+import java.util.Scanner;
 import java.io.*;
 
 
@@ -15,3 +15,19 @@ class GFG {
     }
 }
 
+class PerimeterOfRhombus
+{
+    public static void main(String args[])
+    {
+
+        Scanner s= new Scanner(System.in);
+
+        System.out.println("Enter the side of the Rhombus:");
+
+        double a= s.nextDouble();
+
+        double  perimeter=4*a;
+
+        System.out.println("perimeter of Rhombus is: " + perimeter);
+    }
+}

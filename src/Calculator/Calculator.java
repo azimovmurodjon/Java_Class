b7e92d466b230ffb124f5282d80caabbaa3c5459
M.J. Azimov <96605136+azimovmurodjon@users.noreply.github.com>
2023-09-18 10:36:09 -0400

    Calculate Batting Average
    Using Command Line Arguments

--- src/Calculator/Calculator.java
+++ src/Calculator/Calculator.java
@@ -296,3 +296,39 @@ class BattingAverage2
 
     }
 }
+
+class BattingAverage3
+{
+    public static void main(String args[])
+    {
+        double Mat,inn,notout,avg,run;
+
+        Mat=Double.parseDouble(args[0]);
+
+        inn=Double.parseDouble(args[1]);
+
+        notout=Double.parseDouble(args[2]);
+
+        run=Double.parseDouble(args[3]);
+
+        System.out.println("Matches="+(long)Mat);
+
+        System.out.println("Innings="+(long)inn);
+
+        System.out.println("Notout="+(long)notout);
+
+        System.out.println("Total runs="+(long)run);
+
+        if(inn<=Mat&&notout<=inn)
+        {
+            avg=run /(inn-notout);
+
+            System.out.println("batting average="+avg);
+        }
+        else
+        {
+            System.out.println("please enter innings <= matches and notout<=innings" );
+        }
+
+    }
+}

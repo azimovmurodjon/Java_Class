3c7b5ca495e51900211bcb9b663ae7d2d3ab88f6
M.J. Azimov <96605136+azimovmurodjon@users.noreply.github.com>
2023-10-17 23:31:05 -0400

    Adding Variables Class

---
+++ src/freeCodeCamp/Variables.java
@@ -0,0 +1,36 @@
+package freeCodeCamp;
+
+public class Variables {
+    public static void main(String[] args){
+        int maxValue = Integer.MAX_VALUE;
+        int minValue = Integer.MIN_VALUE;
+        System.out.println("The max value for int is " + maxValue);
+        System.out.println("The min value for int is " + minValue);
+
+        byte bytMaxValue = Byte.MAX_VALUE;
+        byte byteMinValue = Byte.MIN_VALUE;
+
+        System.out.println("The max value for byte is " + bytMaxValue);
+        System.out.println("The min value for byte is " + byteMinValue);
+
+        long longMaxValue = Long.MAX_VALUE;
+        long longMinValue = Long.MIN_VALUE;
+
+        System.out.println("The max value for long is " + longMaxValue);
+        System.out.println("The min value for long is " + longMinValue);
+
+        short shortMaxValue = Short.MAX_VALUE;
+        short shortMinValue = Short.MIN_VALUE;
+
+        System.out.println("The max value for short is " + shortMaxValue);
+        System.out.println("The min value for short is " + shortMinValue);
+
+        double doubleMaxValue = Double.MAX_VALUE;
+        double doubleMinValue = Double.MIN_VALUE;
+
+        System.out.println("The max value for double is " + doubleMaxValue);
+        System.out.println("The min value for double is " + doubleMinValue);
+
+
+    }
+}
